c513fd959c8e41c729f592a163d18b02398026f4
weberjavi <weberjavi@gmail.com>
2021-06-21 12:20:39 +0200

    Avoid using useSWR conditionally

--- src/components/species-modal/expanded-info/expanded-info.js
+++ src/components/species-modal/expanded-info/expanded-info.js
@@ -4,13 +4,12 @@ import useSWR from 'swr';
 
 const ExpandedInfoContainer = (props) => {
   const { speciesName } = props;
-  if (!speciesName) return null;
 
   const fetcher = (url) => (
     fetch(url).then((res) => res.json())
   );
-  const { data, error } = useSWR(
-    `https://api.mol.org/1.x/species/info?scientificname=${speciesName}`,
+  const { data, error } = useSWR((
+    speciesName ? `https://api.mol.org/1.x/species/info?scientificname=${speciesName}` : null),
     fetcher
   );
 
