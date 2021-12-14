a08a259c62c3ae0325a2e7cbaa0a1f5b3ddd263c
weberjavi <weberjavi@gmail.com>
2021-12-14 17:23:25 +0100

    Update search config with new layer

--- src/constants/search-location-constants.js
+++ src/constants/search-location-constants.js
@@ -3,27 +3,34 @@ import {
   GADM_1_ADMIN_AREAS_FEATURE_LAYER,
   GLOBAL_SPI_FEATURE_LAYER,
   WDPA_OECM_FEATURE_LAYER,
+  WDPA_PRECALCULATED_DATA_TABLE,
   } from 'constants/layers-slugs';
+
+import {
+  PROTECTED_AREAS,
+  NATIONAL_BOUNDARIES,
+  SUBNATIONAL_BOUNDARIES,
+} from 'constants/analyze-areas-constants';
 
 import { LAYERS_URLS } from 'constants/layers-urls';
 
 export const SEARCH_SOURCES_CONFIG = {
-  [GADM_0_ADMIN_AREAS_FEATURE_LAYER]: {
+  [NATIONAL_BOUNDARIES]: {
     url: LAYERS_URLS[GADM_0_ADMIN_AREAS_FEATURE_LAYER],
     title: GADM_0_ADMIN_AREAS_FEATURE_LAYER,
     outFields: ["*"],
     searchFields: ["NAME_0", "GID_0"],
     suggestionTemplate: '{NAME_0}'
   },
-  [GADM_1_ADMIN_AREAS_FEATURE_LAYER]: {
+  [SUBNATIONAL_BOUNDARIES]: {
     url: LAYERS_URLS[GADM_1_ADMIN_AREAS_FEATURE_LAYER],
     title: GADM_1_ADMIN_AREAS_FEATURE_LAYER,
     outFields: ["*"],
     searchFields: ["NAME_1", "NAME_0"],
     suggestionTemplate: '{NAME_1}, {NAME_0}'
   },
-  [WDPA_OECM_FEATURE_LAYER]: {
-    url: LAYERS_URLS[WDPA_OECM_FEATURE_LAYER],
+  [PROTECTED_AREAS]: {
+    url: LAYERS_URLS[WDPA_PRECALCULATED_DATA_TABLE],
     title: WDPA_OECM_FEATURE_LAYER,
     outFields: ["*"],
     searchFields: ["NAME"],
