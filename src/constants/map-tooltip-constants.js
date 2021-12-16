5c281b78c0b95919a65831cc951f5f2bd1c373de
weberjavi <weberjavi@gmail.com>
2021-12-16 11:47:28 +0100

    update tooltip config to use both layers

--- src/constants/map-tooltip-constants.js
+++ src/constants/map-tooltip-constants.js
@@ -1,26 +1,31 @@
 import {
   GADM_0_ADMIN_AREAS_FEATURE_LAYER,
   GADM_1_ADMIN_AREAS_FEATURE_LAYER,
-  WDPA_OECM_FEATURE_LAYER,
   WDPA_PRECALCULATED_DATA_TABLE
 } from 'constants/layers-slugs';
 
+import {
+  NATIONAL_BOUNDARIES,
+  SUBNATIONAL_BOUNDARIES,
+  PROTECTED_AREAS,
+} from 'constants/analyze-areas-constants';
+
 
 const MAP_TOOLTIP_CONFIG = {
-  [GADM_0_ADMIN_AREAS_FEATURE_LAYER] : {
+  [NATIONAL_BOUNDARIES] : {
     title: 'NAME_0',
     buttonText: 'analyze area',
     id: 'MOL_ID',
     dataLayer: GADM_0_ADMIN_AREAS_FEATURE_LAYER,
   },
-  [GADM_1_ADMIN_AREAS_FEATURE_LAYER] : {
+  [SUBNATIONAL_BOUNDARIES] : {
     title: 'NAME_1',
     subtitle: 'NAME_0',
     buttonText: 'analyze area',
     id: 'MOL_ID',
     dataLayer: GADM_1_ADMIN_AREAS_FEATURE_LAYER,
   },
-  [WDPA_OECM_FEATURE_LAYER] : {
+  [PROTECTED_AREAS] : {
     title: 'NAME',
     subtitle: 'ISO3',
     buttonText: 'analyze area',
